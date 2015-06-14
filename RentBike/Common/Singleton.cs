c611fd7f404006e09437efc34e0971fc97069fc4
jun8124 <jun8124@gmail.com>
2015-06-14 06:48:03 +0000

    Change singleton file name

--- RentBike/Common/Singleton.cs
+++ RentBike/Common/Singleton.cs
@@ -8,9 +8,10 @@ namespace RentBike.Common
 {
     public class Singleton
     {
+        private static readonly string _singletonFile = "batch." + DateTime.Today.ToString("yyyyMMdd") + ".sg";
         public static void CreateSingletonFile()
         {
-            string fileName = HttpContext.Current.Server.MapPath("~/") + "/logs/" + Constants.SINGLETON_FILE;
+            string fileName = HttpContext.Current.Server.MapPath("~/") + "/logs/" + _singletonFile;
             // Create a file to write to. 
             using (StreamWriter sw = File.CreateText(fileName))
             {
@@ -20,13 +21,13 @@ namespace RentBike.Common
 
         public static bool IsRunningBatch()
         {
-            string fileName = HttpContext.Current.Server.MapPath("~/") + "/logs/" + Constants.SINGLETON_FILE;
+            string fileName = HttpContext.Current.Server.MapPath("~/") + "/logs/" + _singletonFile;
             return File.Exists(fileName);
         }
 
         public static void DeleteSingletonFile()
         {
-            string fileName = HttpContext.Current.Server.MapPath("~/") + "/logs/" + Constants.SINGLETON_FILE;
+            string fileName = HttpContext.Current.Server.MapPath("~/") + "/logs/" + _singletonFile;
             File.Delete(fileName);
         }
     }
