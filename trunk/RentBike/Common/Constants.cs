bd1ad4ab8b17d3f3a21df4e99a67c74bd94206f5
jun8124@gmail.com <jun8124@gmail.com@423e0cff-79c1-6d40-1440-d871321b5fc5>
2015-06-14 06:39:30 +0000

    Singleton for batch processing.

--- trunk/RentBike/Common/Constants.cs
+++ trunk/RentBike/Common/Constants.cs
@@ -23,5 +23,7 @@ namespace RentBike.Common
         public const string ACTION_CREATE_TYPE = "Tạo danh mục";
 
         public const string ACTION_CREATE_INOUT = "Thu/Chi";
+
+        public const string SINGLETON_FILE = "batch.sg";
     }
 }
